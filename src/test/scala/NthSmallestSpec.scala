f5500145c28217725a11e473e41c7ef441e4e4a5
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-04-18 11:20:57 +0100

    All tests passing

--- src/test/scala/NthSmallestSpec.scala
+++ src/test/scala/NthSmallestSpec.scala
@@ -21,7 +21,7 @@ class NthSmallestSpec extends WordSpec with MustMatchers {
             val caught = intercept[IndexOutOfBoundsException] {
               NthSmallest.calculateSmallest(List(1,2,3,4,5,6,7,8,9), 12)
             }
-            assert(caught.getMessage == "No such element in list")
+            assert(caught.toString == "java.lang.IndexOutOfBoundsException: 11")
           }
       }
     }
