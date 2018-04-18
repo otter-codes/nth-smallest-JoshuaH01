4903536f43e9c6185263c27afbdbdeacafd2ee62
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-04-18 13:11:30 +0100

    All tests passing, fixed pattern match

--- src/main/scala/NthSmallest.scala
+++ src/main/scala/NthSmallest.scala
@@ -5,18 +5,15 @@ object NthSmallest extends App {
   def calculateSmallest(list: List[Int], n: Int): Int = {
    list.isEmpty match {
 
-      case false =>
+      case false => if (list.length >= n) {
         val order = list.toSet.toList.sorted.reverse
         println(order)
         val nth = order(n-1)
-        nth
+        nth} else {
+        throw new IndexOutOfBoundsException("Not that many items in the list")}
 
       case true =>
         throw new IllegalArgumentException("list is empty")
-
-      case _ =>
-        throw new IndexOutOfBoundsException("No such element in list")
-
     }
   }
   println(calculateSmallest(List(1,2,3,4,5),10))
