47edc0f84f4cc34506b440b40a2ef7fc1c525619
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-04-18 11:01:16 +0100

    2 test passing, throw argument is not

--- src/main/scala/NthSmallest.scala
+++ src/main/scala/NthSmallest.scala
@@ -1,22 +1,25 @@
+
+
 object NthSmallest extends App {
 
   def calculateSmallest(list: List[Int], n: Int): Int = {
-match list {
+   list.isEmpty match {
 
-      case f => list.nonEmpty
+      case false =>
         val order = list.toSet.toList.sorted.reverse
         println(order)
-        val nth = order(n)
+        val nth = order(n-1)
         nth
 
-      case b => list.isEmpty
+      case true =>
         throw new IllegalArgumentException("list is empty")
 
       case _ =>
-        throw new IllegalArgumentException("No such element in list")
+        throw new IndexOutOfBoundsException("No such element in list")
 
+    }
   }
-  println(calculateSmallest(List(0,1,1,2,3,4,5,6,7,8,9), 2))
+  println(calculateSmallest(List(1,2,3,4,5),10))
 }
 
 
--- src/test/scala/NthSmallestSpec.scala
+++ src/test/scala/NthSmallestSpec.scala
@@ -6,20 +6,20 @@ class NthSmallestSpec extends WordSpec with MustMatchers {
 
     "calculateSmallest is called" should {
       "return a number from the list" in {
-        NthSmallest.calculateSmallest(List(0, 1, 2), 1) mustEqual 1
+        NthSmallest.calculateSmallest(List(0, 1, 2), 1) mustEqual 2
       }
     }
 
     "calculateSmallest is called" should {
       "return a specified number from the list" in {
-        NthSmallest.calculateSmallest(List(1,2,3,4,5,6,7,8,9), 3) mustEqual 6
+        NthSmallest.calculateSmallest(List(1,2,3,4,5,6,7,8,9), 3) mustEqual 7
       }
     }
 
         "calculateSmallest is called" should {
           "throw an error when element is not in the list" in {
-            val caught = intercept[IllegalArgumentException] {
-              NthSmallest.calculateSmallest(List(1,2,3,4,5,6,7,8,9), 3)
+            val caught = intercept[IndexOutOfBoundsException] {
+              NthSmallest.calculateSmallest(List(1,2,3,4,5,6,7,8,9), 12)
             }
             assert(caught.getMessage == "No such element in list")
           }
